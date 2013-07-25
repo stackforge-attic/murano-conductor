6127bb15c7ae1d7b6645f75cb0ecb139cf4f909a
Serg Melikyan <smelikyan@mirantis.com>
2013-07-25 18:42:24 +0400

    Migrate to Murano Common
    Change-Id: I02ef05391f14b489fe8510e80cd4a9b9a516e773

--- setup.py
+++ setup.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python
-# Copyright (c) 2010 OpenStack, LLC.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Copyright (c) 2013 Mirantis, Inc.
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 import setuptools
 
-from conductor.openstack.common import setup
-
-requires = setup.parse_requirements()
-depend_links = setup.parse_dependency_links()
-project = 'conductor'
 
 setuptools.setup(
-    name=project,
-    version=setup.get_version(project, '2013.1'),
-    description='The Conductor is orchestration engine server',
-    license='Apache License (2.0)',
-    author='Mirantis, Inc.',
-    author_email='openstack@lists.launchpad.net',
-    url='http://conductor.openstack.org/',
-    packages=setuptools.find_packages(exclude=['bin']),
-    test_suite='nose.collector',
-    cmdclass=setup.get_cmdclass(),
-    include_package_data=True,
-    install_requires=requires,
-    dependency_links=depend_links,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-        'Environment :: No Input/Output (Daemon)',
-        'Environment :: OpenStack',
+    setup_requires=[
+        'd2to1>=0.2.10,<0.3',
+        'pbr>=0.5,<0.6'
     ],
-    scripts=['bin/conductor'],
-    py_modules=[]
+    d2to1=True,
 )
