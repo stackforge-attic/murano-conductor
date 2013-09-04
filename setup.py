612e80f78d66a2572f237e6c48773886ad511b77
Ekaterina Fedorova <efedorova@mirantis.com>
2013-09-04 18:51:23 +0400

    Change version prefix to 0.2
    Also pbr version was synchronized with other murano compoments to avoid pip conflicts
    
    Change-Id: I5222146695513b6be7a9ef6c981f6b9905a53dff

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import setuptools
 setuptools.setup(
     setup_requires=[
         'd2to1>=0.2.10,<0.3',
-        'pbr>=0.5.21,<1.0'
+        'pbr>=0.5,<0.6'
     ],
     d2to1=True,
     pbr=True
