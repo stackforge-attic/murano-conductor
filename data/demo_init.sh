d3315d07e1ed9143286636371de1aa7ce0d01769
Dmitry Teselkin <dteselkin@mirantis.com>
2013-10-16 17:55:29 +0400

    Typo fixed
    Restart murano-agent added.
    
    Change-Id: Ic2a80a2bae8194530b1e8e908d146062cf72ab03

--- data/demo_init.sh
+++ data/demo_init.sh
@@ -4,4 +4,7 @@ AgentConfigBase64='%AGENT_CONFIG_BASE64%'
 
 mkdir /etc/murano
 
-echo AgentConfigBase64 | base64 -d > /etc/murano/agent.config
+echo $AgentConfigBase64 | base64 -d > /etc/murano/agent.config
+
+/etc/init.d/murano-agent restart
+
