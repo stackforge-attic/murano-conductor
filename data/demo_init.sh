2c3964ce20c8a59d8ed9e9499ed9836d4454df77
Dmitry Teselkin <dteselkin@mirantis.com>
2013-10-16 17:55:29 +0400

    Typo fixed
    Change-Id: Ic2a80a2bae8194530b1e8e908d146062cf72ab03

--- data/demo_init.sh
+++ data/demo_init.sh
@@ -4,4 +4,4 @@ AgentConfigBase64='%AGENT_CONFIG_BASE64%'
 
 mkdir /etc/murano
 
-echo AgentConfigBase64 | base64 -d > /etc/murano/agent.config
+echo $AgentConfigBase64 | base64 -d > /etc/murano/agent.config
