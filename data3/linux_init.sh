3add0e09bccf8590994465e7b91ca35566a85c65
Igor Yozhikov <iyozhikov@mirantis.com>
2013-11-20 19:59:38 +0400

    Changed config path and xinet dist based exec
    Change-Id: Icbfd7b3f68aafc6efd0d4d7e9e8217c4556055e4

--- data3/linux_init.sh
+++ data3/linux_init.sh
@@ -3,7 +3,7 @@
 service murano-agent stop
 
 AgentConfigBase64='%AGENT_CONFIG_BASE64%'
-echo $AgentConfigBase64 | base64 -d > /etc/murano-agent.conf
+echo $AgentConfigBase64 | base64 -d > /etc/murano/agent.conf
 
 # CA-certificate base64 encoded by murano-conductor
 if [ -n "%CA_ROOT_CERT_BASE64%" ]; then
@@ -11,7 +11,7 @@ if [ -n "%CA_ROOT_CERT_BASE64%" ]; then
 %CA_ROOT_CERT_BASE64%
 EOF
  cat /etc/cacert.base64 | base64 -d > /etc/cacert.pem
- sed -i 's/ca_certs.*=/ca_certs=\/etc\/cacert.pem/' /etc/murano-agent.conf
+ sed -i 's/ca_certs.*=/ca_certs=\/etc\/cacert.pem/' /etc/murano/agent.conf
  chmod 644 /etc/cacert.pem
 
  rm /etc/cacert.base64
