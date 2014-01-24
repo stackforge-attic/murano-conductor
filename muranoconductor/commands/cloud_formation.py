12a0d7daed9673f2007dc5ff69e2ec0dc22bbb8a
Ekaterina Fedorova <efedorova@mirantis.com>
2014-01-25 00:12:37 +0400

    Return external network id together with routerId
    blueprint auto-assign-floating-ip
    Change-Id: I4bf9f3be2a09c93360fdda7c96526782225d5bda

--- muranoconductor/commands/cloud_formation.py
+++ muranoconductor/commands/cloud_formation.py
@@ -151,7 +151,7 @@ class HeatExecutor(CommandBase):
                     stack_name=self._stack,
                     parameters=arguments,
                     template=template,
-                    disable_rollback=False)
+                    disable_rollback=True)
 
                 log.debug('Waiting for the stack {0} to be create'.format(
                     self._stack))
